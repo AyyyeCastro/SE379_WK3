fbe7e143a33b92e81364bba89e2a9bf5b7b49ba9
Andrew <a.castro.official@gmail.com>
2023-04-14 15:28:47 -0400

    Version 1 Assignment Submission

--- week3/src/App.jsx
+++ week3/src/App.jsx
@@ -1,35 +1,151 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // textcolor & setTextColor params -> default color black;
+  const [textColor, setTextColor] = useState("#000");
+
+  // if red btn clicked -> red text.
+  const handleRedBtnClick = () => {
+    setTextColor("#ff0000");
+    alert("Red text enabled.");
+  };
+
+  // if blue btn clicked -> blue text.
+  const handleBlueBtnClick = () => {
+    setTextColor("#0000FF");
+    alert("Blue text enabled.");
+  };
 
   return (
-    <div className="App">
+    // I was a little confused by how you wanted us to show the steps, sorry. :( 
+    // First div shows boxes small -> large
+    // Second div shows text small -> large
+    // third div combines them all 
+    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Box size="small"></Box>
+        <Box size="medium"></Box>
+        <Box size="large"></Box>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
 
-export default App
+      <div>
+        <p><Text size="small" color={textColor}>
+          Small Text
+        </Text></p>
+        
+
+        <p><Text size="medium" color={textColor}>
+          Medium Text
+        </Text></p>
+
+        <p><Text size="large" color={textColor}>
+          Large Text
+        </Text></p>
+      </div>
+
+      <div>
+        <Box size="small">
+          <Text size="small" color={textColor}>
+            Small Box
+          </Text>
+        </Box>
+        <Box size="medium">
+          <Text size="medium" color={textColor}>
+            Medium Box
+          </Text>
+        </Box>
+        <Box size="large">
+          <Text size="large" color={textColor}>
+            Large Box
+          </Text>
+        </Box>
+      </div>
+
+      <div>
+        <Button color="red" onClick={handleRedBtnClick}>
+          Click me!
+        </Button>
+        <Button color="blue" onClick={handleBlueBtnClick}>
+          Click me!
+        </Button>
+      </div>
+    </>
+  );
+};
+
+// switch cases for size = widths BOXES
+const Box = (props) => {
+  let width;
+  switch (props.size) {
+    case "small":
+      width = "150px";
+      break;
+    case "medium":
+      width = "250px";
+      break;
+    case "large":
+      width = "350px";
+      break;
+    default:
+      width = "250px";
+  }
+
+  // style for the boxes. Set width = to the variable width, and set border radius.
+  const boxStyle = {
+    width: width,
+    // if not true, then apply 0px default.
+    borderRadius: props.borderRadius ? "15px" : "0px",
+  };
+
+  return (
+    <div className="box" style={boxStyle}>
+      {props.children}
+    </div>
+  );
+};
+
+// switch cases for FONT size
+const Text = (props) => {
+  let fontSize;
+  switch (props.size) {
+    case "small":
+      fontSize = "15px";
+      break;
+    case "medium":
+      fontSize = "20px";
+      break;
+    case "large":
+      fontSize = "25px";
+      break;
+    default:
+      fontSize = "12px";
+  }
+
+  const textStyle = {
+    fontSize: fontSize,
+    color: props.color || "#000000",
+  };
+
+  return <span style={textStyle}>{props.children}</span>;
+};
+
+const Button = ({ color, onClick, children }) => {
+  const buttonStyle = {
+    backgroundColor: color || "#808080",
+    padding: "10px",
+    borderRadius: "5px",
+    color: "white",
+    cursor: "pointer",
+  };
+
+  return (
+    <button style={buttonStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default App;
